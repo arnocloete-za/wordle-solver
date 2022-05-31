f957c28658cf8042183b8634be6c62c30b5d5364
Arno  [Administrator] <arno@zetra.co.za>
2022-05-31 14:58:26 +0200

    added most common characters

--- wordle.py
+++ wordle.py
@@ -38,7 +38,85 @@ for line in all_words:
         continue
     list_of_words.append(line)
 
+vowels_dictionary = {
+    'A': 0,
+    'E': 0,
+    'I': 0,
+    'O': 0,
+    'U': 0,
+    'Y': 0,
+}
 
+const_dictionary = {
+    'B': 0,
+    'C': 0,
+    'D': 0,
+    'F': 0,
+    'G': 0,
+    'H': 0,
+    'J': 0,
+    'K': 0,
+    'L': 0,
+    'M': 0,
+    'N': 0,
+    'P': 0,
+    'Q': 0,
+    'R': 0,
+    'S': 0,
+    'T': 0,
+    'V': 0,
+    'W': 0,
+    'X': 0,
+    'Z': 0,
+}
+
+# count characters in words remaining
+# available words stats:
+for word in list_of_words:
+    for w in word:
+        for key in vowels_dictionary:
+            # print(key)
+            # print(this_dictionary[key])
+            if w == key:
+                vowels_dictionary[key] += 1
+
+    # get list of most characters available left in word_list
+    max_char = 0
+    max_char_list = []
+    for key in vowels_dictionary:
+        if vowels_dictionary[key] > max_char:
+            max_char = vowels_dictionary[key]
+            max_char_list.clear()
+            max_char_list.append(key)
+        elif vowels_dictionary[key] == max_char:
+            max_char_list.append(key)
+
+print("Vowels that are most common in 5 letter English words are:")
+print(max_char_list)
+
+# count characters in words remaining
+# available words stats:
+for word in list_of_words:
+    for w in word:
+        for key in const_dictionary:
+            # print(key)
+            # print(this_dictionary[key])
+            if w == key:
+                const_dictionary[key] += 1
+
+    # get list of most characters available left in word_list
+    max_char = 0
+    max_char_list = []
+    for key in const_dictionary:
+        if const_dictionary[key] > max_char:
+            max_char = const_dictionary[key]
+            max_char_list.clear()
+            max_char_list.append(key)
+        elif const_dictionary[key] == max_char:
+            max_char_list.append(key)
+
+print("Consonants that are most common in 5 letter English words are:")
+print(max_char_list)
 
 #
 #
